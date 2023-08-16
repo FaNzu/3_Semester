e906d7ff6e3680919cc4d7c8841911bfad4b8ee8
AskFanzu <ask.hylleberg@outlook.dk>
2023-08-16 23:04:17 +0200

    encoding on enigma working

--- HjemmeOpgaver/Enigma/Enigma/Enigma.py
+++ HjemmeOpgaver/Enigma/Enigma/Enigma.py
@@ -1,44 +1,67 @@
-import sys
-import math
-
 operation = 'ENCODE'
+#operation = 'DECODE'
 shift = 4
 rotors = ['BDFHJLCPRTXVZNYEIWGAKMUSQO', 'AJDKSIRUXBLHWTMCQGZNPYFVOE', 'EKMFLGDQVZNTOWYHXUSPAIBRCJ']
-
 message = 'AAA'
 result = ''
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'*3
 
-def CaeserEncode(message, shift):
-	alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'*3
+def caeser_encode(message, shift):
+	global alphabet
 	result = [alphabet[alphabet.index(letter)+shift+i] for i, letter in enumerate(message)]
+	print(result)
 	return result
 
-def dorotor(message, rotor):
-	#return ''.join(rotor[ord(c) - ord('A')] for c in message)
-	temp = ''
+def caeser_decode(messagecoded, shift):
+	global alphabet
+	result = [alphabet[alphabet.index(letter)-shift-i] for i, letter in enumerate(message)]
+	return result
+
+
+
+def rotor_func(c: str, rotor: str):
+	return rotor[alphabet.index(c)]
+
+def do_rotor(message, rotor):
+	res = ''
 	for letter in message:
-		temp.join(rotor[ord(letter) - ord('A')])
-	return temp
+		res += rotor_func(letter, rotor)
+	return res
 
+#[abc[cipher.index(c)] for c in 'KLMXYZ']
+#['A', 'B', 'C', 'N', 'O', 'P']
 
-	#alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	#for letter in message:
-	#	result=[rotor[alphabet.index(letter)]]
-	#	print(result)
-	#return result
+def alpabet_func(c:str, rotor:str):
+	return alphabet[rotor.index(c)]
 
-def encode(message, shift, operation):
-	message = CaeserEncode(message, shift)
+def de_do_rotor(coded_message, rotor):
+	res='' 
+	for letter in coded_message:
+		res += alpabet_func(letter, rotor)
+		print(res)
+	return res
+
+def encode(message, shift):
+	message = caeser_encode(message, shift)
 	for rotor in rotors:
-		message = dorotor(message, rotor)
+		message = do_rotor(message, rotor)
 	return message
 	
-def decode():
-	pass
+def decode(message, shift):
+	message = caeser_decode(message, shift)
+	print(message)
+	rotors.reverse()
+	print(rotors)
+	for rotor in rotors:
+		message = de_do_rotor(message, rotor)
+	return message
 
-if operation == 'ENCODE':
-	result = encode(message, shift, operation)
-else:
-	result = decode(message, shift, operation)
+def solution():
 
-print(''.join(result))
+	if operation == 'ENCODE':
+		result = encode(message, shift)
+	else:
+		result = decode(message, shift)
+	print(''.join(result))
+
+solution()
