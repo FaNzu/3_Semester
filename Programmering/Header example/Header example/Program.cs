d8680128250c8827d545d2768cddfa1441ced091
Ask <34611138+FaNzu@users.noreply.github.com>
2023-12-04 11:29:05 +0100

    adding cors

--- Programmering/Header example/Header example/Program.cs
+++ Programmering/Header example/Header example/Program.cs
@@ -9,6 +9,28 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
+//builder.Services.AddCors(options =>
+//{
+//	options.AddPolicy(name: test,
+//		policy =>
+//		{
+//			policy.WithOrigins("http://dr.dk");
+
+//			policy.WithMethods("GET")
+//			.AllowAnyHeader();
+
+//		});
+//}); // her er det kun http://dr.dk/ som kun har lov til at bruge "GET" men alle header
+//app.UseCors(); skal bruges til den koden set over
+
+app.UseCors(options =>
+{
+	options.AllowAnyOrigin();
+	options.AllowAnyMethod();
+	options.AllowAnyHeader();
+}); // dette tillader alle adgang og kan bruge alle metoder
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
